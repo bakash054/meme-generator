2dc70a4e98bb4ab428c278fadcea34041bf64ba4
bakash054 <bakash054@gmail.com>
2026-07-14 08:56:54 +0530

    Added extension to Header import

--- src/memeGenerator/MemeGen.jsx
+++ src/memeGenerator/MemeGen.jsx
@@ -1,6 +1,6 @@
 import './MemeGen.css'
 import Meme from './components/Meme';
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 
 export default function MemeGen(){
     return(
